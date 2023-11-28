23acbe4f4a2112805f964fb94f285582ac6c6059
ColeFrownfelter <144268734+ColeFrownfelter@users.noreply.github.com>
2023-11-28 11:41:34 -0800

    more

--- Assets/Scripts/TurretProjectile.cs
+++ Assets/Scripts/TurretProjectile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour
+public class TurretProjectile : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/TurretTower.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if(player)
+        {
+            //player = other.gameObject;
+            //target position = player.transform.position
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer code
+        //if (timer < 0) ;
+    }
+}
